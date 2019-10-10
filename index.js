ba8fde3c5cdf33fae0ea49181905ec0139afa0c1
Jane Venter <janeanneventer@gmail.com>
2019-10-10 16:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const prepend = ['Milo', 'Garfield', 'Otis'];
 const append = ['Milo', 'Garfield', 'Otis'];
 const removeFirst = ['Milo', 'Garfield', 'Otis'];
 const removeLast = ['Milo', 'Garfield', 'Otis'];
+prepend.unshift('Odie');
+append.push('Odie');
+removeLast.pop();
+removeFirst.shift();
