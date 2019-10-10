462a44afc0f85e9136aee998589543554cd104fc
Jane Venter <janeanneventer@gmail.com>
2019-10-10 16:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your solution here!
+const prepend = ['Milo', 'Garfield', 'Otis'];
+const append = ['Milo', 'Garfield', 'Otis'];
+const removeFirst = ['Milo', 'Garfield', 'Otis'];
+const removeLast = ['Milo', 'Garfield', 'Otis'];
+
